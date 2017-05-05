80a1dd5f68657e7e952c9a0fbb4d260bfcdb2be8
QuodEratDemon <nidhoqed@gmail.com>
2017-05-04 23:56:30 -0700

    state change

--- game/js/main.js
+++ game/js/main.js
@@ -1,44 +1,49 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-var player1;
-var Player1SpawnX = 325;
-var Player1SpawnY = 200; 
+var mainState = {
 
-var player2;
-var Player2SpawnX = 475;
-var Player2SpawnY = 200;
+	preload: function() {
+	 	game.load.spritesheet('player','assets/img/dude.png', 32, 48);
+	 	var player1;
+		var Player1SpawnX = 325;
+		var Player1SpawnY = 200; 
 
-//AG: Collider group vars
-var players;
-var ground;
+		var player2;
+		var Player2SpawnX = 475;
+		var Player2SpawnY = 200;
 
-function preload() {
- game.load.spritesheet('player','assets/img/dude.png', 32, 48);}
+		//AG: Collider group vars
+		var players;
+		var ground;
+	}
 
-function create() {
-    var player1 = new Player(game, 'player', Player1SpawnX, Player1SpawnY, 1);
-    game.add.existing(player1);
-    
-    var player2 = new Player(game, 'player', Player2SpawnX, Player2SpawnY, 2);
-    game.add.existing(player2);
-    
-    //AG: Attempt to get physics working
-    var plat = game.add.sprite(0, game.height-32, 'player');
-    game.physics.enable(plat);
-    plat.body.immovable = true;
-    plat.scale.setTo(25,1);
-    
-    //AG: Physics from http://www.codevinsky.com/phaser-2-0-tutorial-flappy-bird-part-2/
-    game.physics.startSystem(Phaser.Physics.Arcade);
-    players = this.game.add.group();
-    players.add(player1);
-    players.add(player2);
-    
-    ground = this.game.add.physicsGroup();
-    ground.add(plat);
-}
+	create: function() {
+	    var player1 = new Player(game, 'player', Player1SpawnX, Player1SpawnY, 1);
+	    game.add.existing(player1);
+	    
+	    var player2 = new Player(game, 'player', Player2SpawnX, Player2SpawnY, 2);
+	    game.add.existing(player2);
+	    
+	    //AG: Attempt to get physics working
+	    var plat = game.add.sprite(0, game.height-32, 'player');
+	    game.physics.enable(plat);
+	    plat.body.immovable = true;
+	    plat.scale.setTo(25,1);
+	    
+	    //AG: Physics from http://www.codevinsky.com/phaser-2-0-tutorial-flappy-bird-part-2/
+	    game.physics.startSystem(Phaser.Physics.Arcade);
+	    players = this.game.add.group();
+	    players.add(player1);
+	    players.add(player2);
+	    
+	    ground = this.game.add.physicsGroup();
+	    ground.add(plat);
+	}
 
-function update() {
-    game.physics.arcade.collide(players,ground); 
-}
+	update: function() {
+	    game.physics.arcade.collide(players,ground); 
+	    game.physics.arcade.collide(players,players); 
+	}
   
+
+}
